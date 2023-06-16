8877346ac8b9031bc3a1ba4f5f43f2be11125c4b
Everton Pires <epe.masork@hotmail.com>
2023-06-15 23:23:27 -0300

    feat: create repository

---
+++ src/main/java/com/evrtonpires/springboot/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.evrtonpires.springboot.repositories;
+
+import com.evrtonpires.springboot.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+
+}
